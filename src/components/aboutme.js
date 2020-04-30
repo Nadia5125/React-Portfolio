45e106e4a7538bff9cc223b39df5d35b39839ffb
Nadia <nadiamiswiyati@gmail.com>
2020-04-30 18:59:43 -0400

    Added on contact and about page

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-
+import './about.css';
 class About extends Component {
   render() {
     return(
-      <div style={{width: '50%', position: 'relative', left: '400px', top: '30px'}}>
+      <div className="container" style={{width: '50%', top: '30px'}}>
         <div>
           <h1 style={{textAlign: 'center', fontFamily: 'Italic', color: '#4B79A1'}}>About Nadia</h1>
         <hr/>
-        <p>I always have been fascinated by how technology is involved in our daily lives and makes our lives easier and more productive. It also connects people globally who were not connected in the past without all of the latest that tech has to offer. And I am currently learning Full Stack Bootcamp at Gorge Washington University.</p>
+        <p style={{padding: '30px', fontSize:'medium', margin: '10px'}}>I always have been fascinated by how technology is involved in our daily lives and makes our lives easier and more productive. It also connects people globally who were not connected in the past without all of the latest that tech has to offer. And I am currently learning Full Stack Bootcamp at Gorge Washington University.</p>
         </div>   
       </div>
       
